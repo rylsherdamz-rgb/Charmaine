f46dc093970a1550101ee8b4fc2e27940858e0b2
rylsherdamz-rgb <rylsherdamz@gmail.com>
2026-06-02 13:41:40 +0800

    move folders into src

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,14 @@
+import { View, Text, Image } from "react-native"
+
+// this the app header component that will be use in theme
+
+
+
+export default function AppHeader() {
+    return (
+    <View className="w-full h-full rounded-2xl  py-2  px-5">
+            // add more ui design in here
+
+    </View>
+    )
+}
