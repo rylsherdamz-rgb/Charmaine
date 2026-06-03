0336b59f3a4161ba262ace9e71d729ff1e3ba43e
rylsherdamz-rgb <rylsherdamz@gmail.com>
2026-06-03 22:45:10 +0800

    added more items

---
+++ src/hooks/useAudiot.tsx
@@ -0,0 +1,14 @@
+import * as Audio from "expo-audio"
+
+
+export default function useAudio() {
+    // import neccessary function read the docs for now
+
+    try {
+
+    } catch (error) {
+
+    }
+
+    return
+}
--- src/hooks/useLocalLLM.tsx
+++ src/hooks/useLocalLLM.tsx
@@ -1,1 +1,7 @@
 import { useState } from "react";
+
+
+
+export default function useLocalLLM() {
+
+}
