3e9dc93090a1dc928d4f4679b6078e467c2d8792
rylsherdamz-rgb <rylsherdamz@gmail.com>
2026-06-04 12:32:08 +0800

    fix complicting packages now building dev build

--- src/app/File/FileView.tsx
+++ src/app/File/FileView.tsx
@@ -1,6 +1,9 @@
 import { Pressable, View, Text } from "react-native"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 
+
+// this is all of the file that have been uploaded will shows
+
 export default function FileView() {
     const insets = useSafeAreaInsets()
     return <View
